a7e3c775c6e629fdb68fc6256d13de430edf25e8
Alvin Mun <73019337+AlvinMun@users.noreply.github.com>
2025-04-16 18:44:18 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# TODO-1: Ask the user for input
+# TODO-2: Save data into dictionary {name: price}
+# TODO-3: Whether if new bids need to be added
+# TODO-4: Compare bids in dictionary
+from art import logo
+print(logo)
+
+def find_highest(record):
+    highest_bid = 0
+    winner = ""
+    for key in record:
+        amount = record[key]
+        if amount > highest_bid:
+            highest_bid = amount
+            winner = key
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+auction = {}
+should_continue = True
+while should_continue:
+    name = input("What is your name?: ")
+    bid = int(input("What is your bid?: $"))
+    yes_or_no = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+    auction[name] = bid
+    if yes_or_no == 'yes':
+        print("\n" * 100)
+        continue
+    if yes_or_no == 'no':
+        should_continue = False
+        find_highest(auction)
